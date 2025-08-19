1b771f48ee43301c371cc251bbd40e3743026ff1
kdj <64907633+childevnote@users.noreply.github.com>
2025-08-20 00:11:10 +0900

    08-20-2025 2348-number-of-zero-filled-subarrays .py Medium 45 ms (36.89%) 28.7 MB (6.21%)

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -1,11 +1,22 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
+        arr = []
+        streakFlag = False
         streak = 0
         result = 0
         for z in nums:
             if z == 0:
                 streak += 1
-                result += streak
-            else:
+                streakFlag = True
+            if z != 0 and streakFlag == True:
+                arr.append(streak)
                 streak = 0
+                streakFlag = False
+
+        if streak:
+            arr.append(streak)
+
+        for a in arr:
+            result += a*(a+1)//2
+
         return result
