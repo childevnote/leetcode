371713f32c5681b279ed8bf84c2ec6a6384db475
kdj <64907633+childevnote@users.noreply.github.com>
2025-09-16 21:43:12 +0900

    Time: 0 ms (100%), Space: 17.8 MB (42.07%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,9 +1,11 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if not s:
-            return True  # s가 빈 문자열이면 항상 True
+
+        if len(s) == 0:
+            return True
         index = 0
         for w in t:
-            if index < len(s) and w == s[index]:
+            if index < len(s) and w == s[index] :
                 index += 1
-        return index == len(s)
+        
+        return True if len(s)==index else False
